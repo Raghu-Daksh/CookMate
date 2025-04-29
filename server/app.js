4f5fe50c4e6252672d8e3d47f3d5aab2ab155a47
Raghu Daksh <71201937+Raghu-Daksh@users.noreply.github.com>
2025-04-29 20:53:34 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,19 +12,6 @@ app.use(express.json()); // Middleware to parse JSON requests
 app.use('/user', userRouter)
 app.use('/contact', userContactRouter)
 
-// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-// // Catch-all route for React
-// app.get('*', (req, res) => {
-//     const filePath = path.join(__dirname, '..', 'client', 'build', 'index.html');
-//     console.log(`Serving file: ${filePath}`);
-//     if (fs.existsSync(filePath)) {
-//         res.sendFile(filePath);
-//     } else {
-//         res.status(404).send('index.html not found');
-//     }
-// });
-
 app.listen(port, ()=>{
     console.log("Server is running on port 5000");
 });
